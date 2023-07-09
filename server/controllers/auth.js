1afe97c6b4bc7d28cb67a521376e055e6cb8d3c4
ImB03 <giabao7803@gmail.com>
2023-07-09 22:19:02 +0700

    upload expires 2h

--- server/controllers/auth.js
+++ server/controllers/auth.js
@@ -61,7 +61,7 @@ export const signin = async (req, res) => {
       { email: existingUser.email, id: existingUser._id },
       process.env.SECRET_KEY,
       {
-        expiresIn: "20s",
+        expiresIn: "2h",
       }
     );
 
